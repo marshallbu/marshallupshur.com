f28d5a3839ae730157fb02c01a4a7fa5eae2cfc0
Marshall Upshur <marshall@mbucreations.com>
2014-08-05 21:18:40 -0700

    more gulp tasks

--- gulp/tasks/mincss.js
+++ gulp/tasks/mincss.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp'),
+    isProduction = process.env.NODE_ENV === 'production',
     plugins = require('gulp-load-plugins')(),
     config = require('./../utils/config');
 
 gulp.task('mincss', ['less'], function() {
     return gulp.src(config.dist.dir.styles + '*.css')
-        .pipe(plugins.minifyCss())
+        .pipe(plugins.if(isProduction, plugins.minifyCss()))
         .pipe(gulp.dest(config.dist.dir.styles));
 });
---
+++ gulp/tasks/minhtml.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+    isProduction = process.env.NODE_ENV === 'production',
+    plugins = require('gulp-load-plugins')(),
+    config = require('./../utils/config');
+
+gulp.task('minhtml', ['compile-html'], function() {
+    var opts = {comments:true,spare:true};
+
+    return gulp.src(config.distRoot + '*.html')
+        .pipe(plugins.if(isProduction, plugins.minifyHtml(opts)))
+        .pipe(gulp.dest(config.distRoot));
+});
