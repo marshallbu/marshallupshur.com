03f1b6c375a1fdf2d517ce46ff35bf7a1f715741
Marshall Upshur <marshall@mbucreations.com>
2014-12-13 13:51:55 -0800

    changing nav bar to ngclicks

--- app/scripts/controllers/nav_ctrl.js
+++ app/scripts/controllers/nav_ctrl.js
@@ -2,6 +2,10 @@
 var NavCtrl = ['$scope', '$log',
   function ($scope, $log) {
 
+    $scope.scroll = function scroll(anchor) {
+      $('body').stop().scrollTo(anchor, 500);
+    };
+
     // $scope.$apply();
     $log.info('NavCtrl initialized');
   }
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,23 +12,7 @@ angular.element().ready(function() {
 
 $(document).ready(function () {
 
-  // TODO: move to a directive
-  var navLinks;
-
   $('#creations-slider').carousel();
-
-  navLinks = function(clicked) {
-    var itemId;
-    itemId = clicked.attr('href');
-    return $('body').stop().scrollTo(itemId, 500);
-  };
-
-  $('a.scrollto').click(function() {
-    var nav_clicked;
-    nav_clicked = $(this);
-    navLinks(nav_clicked);
-    return false;
-  });
 
   $('[data-spy="scroll"]').each(function () {
     var $spy = $(this).scrollspy('refresh');
