5a37e3fcf28c2e16d143ced016ea4e75414590da
Marshall Upshur <marshall@mbucreations.com>
2014-12-13 18:59:48 -0800

    hide hamburger menu on click

--- app/scripts/controllers/nav_ctrl.js
+++ app/scripts/controllers/nav_ctrl.js
@@ -4,6 +4,7 @@ var NavCtrl = ['$scope', '$log',
 
     $scope.scroll = function scroll(anchor) {
       $('body').stop().scrollTo(anchor, 500);
+      $('#mbu_main_navbar').collapse('hide')
     };
 
     // $scope.$apply();
