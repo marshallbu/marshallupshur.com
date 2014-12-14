b2e6cceedabfc94631974d76235ea7f01cde09be
Marshall Upshur <marshall@mbucreations.com>
2014-12-13 19:16:29 -0800

    setting up some stuff for testing

--- gulp_tasks/tasks/unit-test.js
+++ gulp_tasks/tasks/unit-test.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp'),
+    sequence = require('run-sequence'),
     config = require('./../utils/config');
 
 gulp.task('unit-test', function (cb) {
-
+  // TODO: we all know this isn't a unit test
+  sequence(
+    ['lint'],
+    cb
+  );
 });
