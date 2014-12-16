c7bacb1f262be675763794fec9768a2ca5176656
Marshall Upshur <marshall@mbucreations.com>
2014-12-16 15:20:17 -0800

    getting rid of page flicker on clicking navigation elements when not in collapsed mode

--- app/scripts/controllers/main_ctrl.js
+++ app/scripts/controllers/main_ctrl.js
@@ -1,5 +1,7 @@
 var MainCtrl = ['$rootScope', '$scope', '$log',
   function ($rootScope, $scope, $log) {
+    // will use this to enable/disable functionality in the future
+    $rootScope.switches = {};
 
     $log.info('MainCtrl:initialized');
   }
--- app/scripts/controllers/nav_ctrl.js
+++ app/scripts/controllers/nav_ctrl.js
@@ -1,10 +1,16 @@
 
-var NavCtrl = ['$scope', '$log',
-  function ($scope, $log) {
+var NavCtrl = ['$scope', '$log', '$window',
+  function ($scope, $log, $window) {
 
     $scope.scroll = function scroll(anchor) {
       $('body').stop().scrollTo(anchor, 500);
-      $('#mbu_main_navbar').collapse('hide');
+
+      // TODO: this is dirty, Bootstrap should provide a better way to check if
+      // navbar is in hamburger mode
+      if ($window.innerWidth < 768) {
+        $('#mbu_main_navbar').collapse('hide');
+      }
+
     };
 
     // $scope.$apply();
