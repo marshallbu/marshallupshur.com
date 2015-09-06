0703bd0551ed7769ddfcb137f2c42baf4d9745b9
marshallbu <marshall@mbucreations.com>
2015-09-05 18:57:46 -0700

    little clean up, first prod build ready

--- app/scripts/libs/libs.js
+++ app/scripts/libs/libs.js
@@ -14,6 +14,7 @@ import 'react-bootstrap/lib/Modal';
 import 'moment';
 // import 'react-scroll';
 import './../vendor/rAF';
+import 'viewer';
 import './../vendor/scrollspy.jsx';
 
 // custom greensock namespace
